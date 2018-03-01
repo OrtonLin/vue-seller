30974c60f6c1b97d971a5e3ad40af6f65edbf0b4
ortonlin <jackeir@gmail.com>
2018-03-01 09:31:45 +0800

    Day 7-1. Header (font-icon, import css)

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,6 +1,22 @@
 <<template>
     <div class="header">
-      {{poiInfo.name}}
+      <div class="top-wrapper">
+        <div class="back-wrapper">
+          <span class="icon-arrow_lift"></span>
+        </div>
+        <form class="search-wrapper">
+          <span class="search-icon"></span>
+          <input class="search-bar" type="text" placeholder="搜尋店內商品"/>
+        </form>
+        <div class="more-wrapper"></div>
+      </div>
+      <div class="content-wrapper">
+        <a href="#" class="spelling-bt">拼單</a>
+        <div class="more-bt"></div>
+      </div>
+      <div class="bulletin-wrapper">
+
+      </div>
   </div>
 </template>
 
@@ -16,6 +32,7 @@ export default {
 </script>
 
 <<style>
-
+@import url('Header.css');
+@import url('../../common/styles/icon-style.css');
 </style>
 
